b0e6b26a3cbe388736a4cdf2acee1daf7ef955ed
qinglu@kooboo.cn <qinglu@kooboo.cn>
2017-08-23 17:57:17 +0800

    Init

---
+++ Presentation/HQL.KLNC.Web/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HQL.KLNC.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
